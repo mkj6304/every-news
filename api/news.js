949c3ea8c86ecb5a3ca67970f3e6c2319cfb51e5
Mayank <mayankjha632004@gmail.com>
2025-07-05 13:30:44 +0530

    api issues fixed

---
+++ api/news.js
@@ -0,0 +1,13 @@
+export default async function handler(req, res) {
+  const API_KEY = "your_actual_api_key"; // hardcoded temporarily
+  const { country = "in", category = "general", page = 1, pageSize = 5 } = req.query;
+
+  try {
+    const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&page=${page}&pageSize=${pageSize}&apiKey=${API_KEY}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to fetch news" });
+  }
+}
--- src/Components/News.js
+++ src/Components/News.js
@@ -3,90 +3,91 @@ import NewsItem from './NewsItem';
 import PropTypes from 'prop-types'
 import Spinner from './Spinner';
 
-
 export class News extends Component {
-  static defaultProps={
-      pageSize: 5,
-      country: 'in',
-      category: 'general'
+  static defaultProps = {
+    pageSize: 5,
+    country: 'in',
+    category: 'general'
   }
 
   static propTypes = {
-      pageSize: PropTypes.number,
-      country: PropTypes.string,
-      category: PropTypes.string
+    pageSize: PropTypes.number,
+    country: PropTypes.string,
+    category: PropTypes.string
   }
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1,
+      totalResults: 0
+    };
+    document.title = this.capitalizeFirstLetter(this.props.category);
+  }
 
-    
-  constructor(props){
-        super(props);
-        console.log("Hello I am a constructor");
-        this.state={
-            articles: this.articles,
-            loading: false,
-            page: 1
-        }
-        document.title = this.capitalizeFirstLetter(this.props.category);
-    }
-
-   capitalizeFirstLetter = (val) => {
+  capitalizeFirstLetter = (val) => {
     return String(val).charAt(0).toUpperCase() + String(val).slice(1);
-}
+  }
 
-  async update(){
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    this.setState({loading: true})
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    console.log(parsedData);
-    this.setState({articles: parsedData.articles,
-      totalResults: parsedData.totalResults,
-      loading: false
-    })
-  }  
+  async update() {
+    const { country, category, pageSize } = this.props;
+    const { page } = this.state;
 
-  async componentDidMount(){
+    // 👇 Call your Vercel serverless API endpoint
+    const url = `/api/news?country=${country}&category=${category}&page=${page}&pageSize=${pageSize}`;
+    this.setState({ loading: true });
+
+    try {
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData);
+      this.setState({
+        articles: parsedData.articles,
+        totalResults: parsedData.totalResults,
+        loading: false
+      });
+    } catch (err) {
+      console.error("Failed to fetch news:", err);
+      this.setState({ loading: false });
+    }
+  }
+
+  async componentDidMount() {
     this.update();
   }
 
   handlenextclick = async () => {
-    this.setState({page :this.state.page +1});
-    this.update();
+    this.setState({ page: this.state.page + 1 }, this.update);
   }
 
   handleprevclick = async () => {
-    this.setState({page :this.state.page -1});
-    this.update();
+    this.setState({ page: this.state.page - 1 }, this.update);
   }
 
-    render() {
-    
+  render() {
     return (
-       <div>
-   
-    <div className='container my-3'>
-<h1 className="text-center">Every-News - Top Headlines for {document.title}</h1>
-      {this.state.loading && <Spinner/>}
-    <div className="row">
-    {!this.state.loading && Array.isArray(this.state.articles) && this.state.articles.map((element)=>{
-        return  <div className="col md-4" key={element.url}>
-        <NewsItem title={element.title} description={element.description} imageurl={element.urlToImage} newsUrl={element.url} author={element.author} publishedAt={element.publishedAt} />
+      <div>
+        <div className='container my-3'>
+          <h1 className="text-center">Every-News - Top Headlines for {document.title}</h1>
+          {this.state.loading && <Spinner />}
+          <div className="row">
+            {!this.state.loading && Array.isArray(this.state.articles) && this.state.articles.map((element) => {
+              return <div className="col md-4" key={element.url}>
+                <NewsItem title={element.title} description={element.description} imageurl={element.urlToImage} newsUrl={element.url} author={element.author} publishedAt={element.publishedAt} />
+              </div>
+            })}
+          </div>
+
+          <div className="container d-flex justify-content-between">
+            <button type="button" disabled={this.state.page <= 1} onClick={this.handleprevclick} className="btn btn-dark">Previous</button>
+            <button type="button" disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} onClick={this.handlenextclick} className="btn btn-dark">Next</button>
+          </div>
+        </div>
       </div>
-        
-   
-    })}
-    
-      </div>
-     
-     <div className="container d-flex justify-content-between">
-      <button type="button" disabled={this.state.page<=1} onClick={this.handleprevclick} className="btn btn-dark">Previous</button>
-     <button type="button" disabled={this.state.page+1 > Math.ceil(this.state.totaResults/8)} onClick={this.handlenextclick} className="btn btn-dark">Next</button>
-     </div>
-    </div>
-    </div>
-    )
+    );
   }
 }
 
-export default News
+export default News;
